f27bd43372b614ba6cd6d298ac21c2b35704ab59
Kasun Madhumal <73273550+Kasun-se-2018-023@users.noreply.github.com>
2021-07-13 12:02:28 +0530

    Update Basic Probability.py

--- Day_2/Basic Probability.py
+++ Day_2/Basic Probability.py
@@ -1,8 +1,8 @@
-'''we define E to be the event in which the sum rolled by two evenly-weighted dice is > 9
+we define E to be the event in which the sum rolled by two evenly-weighted dice is > 9
 
 The set of outcomes satisfying E is: {(4,6),(5,5),(6,4),(6,5),(6,6),(5,6)} we can use this to find:
 
 P(E) = |E|/|S| = 6/36 = 1/6
 
 we want to find the probability of sum being <= 9 . Because we hace P(E) and we know that the probability we want
-comprises the rest of the sample space, we calculate it as follows: 1- P(E) = 1-1/6 = 5/6'''
+comprises the rest of the sample space, we calculate it as follows: 1- P(E) = 1-1/6 = 5/6
